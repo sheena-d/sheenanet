e675e33372825f4bcdf4dd8d425edf3f1513b6b2
Sheena Donnelly <sheena.donnelly@gmail.com>
2012-05-08 16:55:38 -0400

    moving font support to typekit, tweaking media queries, tweaking jquery masonry and infinitescroll to work nicely with fonts and media queries.

--- themes/sheenadotnet/js/sheenadotnet-front-script.js
+++ themes/sheenadotnet/js/sheenadotnet-front-script.js
@@ -1,28 +1,65 @@
 (function ($) {
 Drupal.behaviors.sheenaMasonry = {
   attach: function (context, settings) {
+        var $container;
+        var $pgCount = 0;
+        var $pgNum = $('ul.pager li.pager-item').size();
         
-        $('#main').masonry({
-          itemSelector : 'article.node-teaser'
+        $('article.node-teaser .inner').hover(
+         function() {
+           $(this).find('.overlay').fadeIn();
+         },
+         function() {
+           $(this).find('.overlay').fadeOut();
+         }
+         );
+        
+        function triggerMasonry() {
+          if (!$container) {
+            return
+          }
+          $container.masonry({
+            itemSelector : 'article.node-teaser'
+          });
+        }
+        
+        $(function() {
+          $container = $('#main');
+          triggerMasonry();
+        });
+        
+        Typekit.load({
+          active: triggerMasonry,
+          inactive: triggerMasonry
         });
         
         $('#main').infinitescroll({
           navSelector : 'ul.pager',
           nextSelector : '.pager-next a',
-          itemSelector : 'article.node-teaser'
+          itemSelector : 'article.node-teaser',
+          donetext : "nothing to see here..."
         }, 
         function(newElements){
           var $newElems = $( newElements );
+          $pgCount ++;
+          
+          if($pgCount == $pgNum) {
+            $(window).unbind('.infscr');
+            $('#main').append('<div class="scrollEmpty">Nothing more to see here</div>');
+          }
+          
           $('#main').masonry('appended', $newElems);
+          
+          $('article.node-teaser .inner').hover(
+           function() {
+             $(this).find('.overlay').fadeIn();
+           },
+           function() {
+             $(this).find('.overlay').fadeOut();
+           }
+           );
       });
   }
 };
 
-Drupal.behaviors.sheenaArticleOverlay = {
-  attach: function(context, settings) {
-    $('article.node-teaser .overlay').hide();
-    
-    $('article.node-teaser').mouseOver()
-  }
-};
 })(jQuery);
--- themes/sheenadotnet/template.php
+++ themes/sheenadotnet/template.php
@@ -1,13 +1,15 @@
 <?php
 
 function sheenadotnet_preprocess_page(&$vars){
+  drupal_add_js('http://use.typekit.com/iyk3kkc.js', array('type'=>'external', 'preprocess'=>false, 'group'=>'JS_LIBRARY', 'weight'=>-19));
 
   if($vars['is_front']) {
-    drupal_add_js(drupal_get_path('theme', 'sheenadotnet').'/js/jquery.masonry.min.js');
-    drupal_add_js(drupal_get_path('theme', 'sheenadotnet').'/js/jquery.infinitescroll.min.js');
+    drupal_add_js(drupal_get_path('theme', 'sheenadotnet').'/js/jquery.masonry.min.js', array('group'=>'JS_LIBRARY'));
+    drupal_add_js(drupal_get_path('theme', 'sheenadotnet').'/js/jquery.infinitescroll.min.js', array('group'=>'JS_LIBRARY'));
     drupal_add_js(drupal_get_path('theme', 'sheenadotnet').'/js/sheenadotnet-front-script.js');
+  }else{
+    drupal_add_js('try{Typekit.load();}catch(e){}', array('type'=>'inline', 'preprocess'=>false, 'group'=>'JS_LIBRARY', 'weight'=>-19));
   }
-
 }
 
 function sheenadotnet_preprocess_field(&$vars){
@@ -15,10 +17,8 @@ function sheenadotnet_preprocess_field(&$vars){
   }
 
 function sheenadotnet_preprocess_node(&$vars){
-  unset($vars['content']['links']);
-  
-  switch($vars['type']) {
-  
+  unset($vars['content']['links']);  
+  switch($vars['type']) {  
   case('drupal_answers'):  
     
   break;
